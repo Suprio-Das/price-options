c29b83c00062d29ed9fc188ca38244c2660ae6a0
Suprio-Das <priyamd507.gmail.com>
2024-05-27 16:05:13 +0600

    Navbar Created

---
+++ src/Component/Link/Link.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+const Link = ({ route }) => {
+    const { name, path } = route;
+    return (
+        <div>
+            <li className='mr-12 font-semibold hover:bg-black px-3 py-1 hover:text-white hover:rounded-md'><a href={path}>{name}</a></li>
+        </div>
+    );
+};
+
+Link.propTypes = {
+    route: PropTypes.object
+}
+
+export default Link;
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import { ImSpoonKnife } from "react-icons/im";
+
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const routes = [
+        { name: 'Home', id: 1, path: '/' },
+        { name: 'About', id: 2, path: '/about' },
+        { name: 'Contact', id: 3, path: '/contact' },
+        { name: 'User Profile', id: 4, path: '/user/:id' },
+        { name: 'Not Found', id: 5, path: '*' },
+    ];
+    return (
+        <nav className="bg-amber-500 p-3 lg:flex flex-row justify-center items-center">
+            <div className="md:hidden" onClick={() => setOpen(!open)}>
+                {
+                    open === true ? <MdOutlineRestaurantMenu></MdOutlineRestaurantMenu> : <ImSpoonKnife></ImSpoonKnife>
+                }
+            </div>
+            <ul className={`lg:flex ${open ? 'block' : 'hidden'}`}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
